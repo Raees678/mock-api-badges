fb3de258983f5254eb8fcc9e76be249c36ee9bdf
Raees Rajwani <raeesrajwani95@gmail.com>
2020-10-26 23:49:39 +0530

    Mock API built

---
+++ index.js
@@ -0,0 +1,148 @@
+const express = require("express");
+var bodyParser = require("body-parser");
+
+const app = express();
+const port = 3000;
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+app.post("/badge-statistics/", jsonParser, function (req, res) {
+  if (req.body.user_id !== null && req.body.user_id !== undefined && req.body.user_id > 0) {
+    res.send({
+      statistics_response,
+    });
+  } else {
+    res.statusCode = 400;
+    res.send("Invalid request");
+  }
+});
+
+app.post("/date-filtered-badges/", jsonParser, function (req, res) {
+  let req_date = req.body.date ? parseInt(req.body.date) : null;
+  let req_month = req.body.month ? parseInt(req.body.month) : null;
+  let req_year = req.body.year ? parseInt(req.body.year) : null;
+
+  if (
+    req.body.user_id !== null &&
+    req.body.user_id !== undefined &&
+    req.body.user_id > 0 &&
+    req_date &&
+    req_month &&
+    req_year
+  ) {
+    let date = new Date(req_year, req_month - 1, req_date + 1, 0, 0, 0);
+
+    let limit = date.getDay() - 2;
+    limit <= 0 ? (limit += 7) : (limit += 0);
+    let response = date_filtered_badges_response;
+    response.all_badges = [];
+    for (let i = 0; i < limit; ++i) {
+      let badges_for_date = {};
+      Object.assign(badges_for_date, sample_date_badge);
+      badges_for_date.date = new Date(req_year, req_month - 1, req_date - i, 0, 0, 0);
+      response.all_badges.push(badges_for_date);
+    }
+    res.send({ limit: limit, date_filtered_badges_response });
+  } else {
+    res.statusCode = 400;
+    res.send("Invalid request");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+var statistics_response = {
+  weekly_badges: {
+    red: {
+      count: 1,
+    },
+    yellow: {
+      count: 7,
+      reason: ["Late check in daily attendance", "Not completing a single task in a day"],
+    },
+    green: {
+      count: 3,
+      reason: ["Completed more than 80% of assignments in a module", "Completing more than 10 hours in a day"],
+    },
+  },
+  total: {
+    red: {
+      count: 38,
+    },
+    yellow: {
+      count: 240,
+      reason: ["Late check in daily attendance", "Late check in class attendance", "Not completing 10 hours a day"],
+    },
+    green: {
+      count: 40,
+      reason: [
+        "Completed more than 80% of assignments in a module",
+        "On time check in daily attendance",
+        "On time check in class attendance",
+      ],
+    },
+  },
+};
+
+var date_filtered_badges_response = {
+  labels: [
+    {
+      count: 24,
+      badge: "On time check in daily attendance",
+    },
+    {
+      count: 22,
+      badge: "On time check in class attendance",
+    },
+    {
+      count: 12,
+      badge: "Completing 10 hours per day",
+    },
+    {
+      count: 20,
+      badge: "Completed more than 80% of assignments in a week for a module",
+    },
+    {
+      count: 20,
+      badge: "Late check in daily attendance",
+    },
+    {
+      count: 20,
+      badge: "Late check in class attendance",
+    },
+    {
+      count: 20,
+      badge: "Not completing 10 hours/ Early check out",
+    },
+    {
+      count: 20,
+      badge: "Not finishing a single task in a day",
+    },
+  ],
+  all_badges: [],
+};
+
+var sample_date_badge = {
+  date: "",
+  badges: [
+    {
+      event: "On time attendance check in",
+      badge: {
+        color: "green",
+        reason: "Checked in at 9:00:03",
+        issued_at: "2020-10-23T00:00",
+      },
+    },
+    {
+      event: "Late class check in",
+      badge: {
+        color: "yellow",
+        reason: "Checked in at 9:52:46",
+        issued_at: "2020-10-23T00:00",
+      },
+    },
+  ],
+};
